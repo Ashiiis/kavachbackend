fe18e98020f616f4b40e13475ca268c01d831200
Ashiiis <158624214+Ashiiis@users.noreply.github.com>
2024-03-15 22:48:39 +0530

    hahha

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s@(u+@98po^40@@ybh259pv*o13nd2sw+f3_-%+^v^y0&+k*m4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
